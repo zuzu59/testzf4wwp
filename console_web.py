368f77a13f43fd3231e758a35acdb70e746ea4f7
Christian Zufferey <christian@zufferey.com>
2017-07-13 21:59:07 +0200

    - juste pour sauvegarder quelque chose uqi commence à fonctionner ;-) - encore beaucoup de chose à nettoyer !

--- console_web.py
+++ console_web.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 #Petit script python tout simple pour afficher deux iframes côte à côte pour comapraison visuelle de deux sites web
 #zf170713.1558
-#source: https://gist.github.com/bradmontgomery/2219997
+#source: 
+#https://gist.github.com/bradmontgomery/2219997
+#https://gist.github.com/trungly/5889154
 
 
 import socket
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
+import urllib.parse
 
 hostName = ""
 hostPort = 18081
@@ -18,17 +21,29 @@ class MyServer(BaseHTTPRequestHandler):
 		self.send_header('Content-type', 'text/html')
 		self.end_headers()
 
-	#	GET is for clients geting the predi
+
+
 	def do_GET(self):
+
+		#parsed = urlparse.urlparse(url)
+		parsed_path = urllib.parse.urlparse(self.path)
+		print(parsed_path)
+		query_params = urllib.parse.parse_qs(parsed_path.query)
+		if 'url1' in query_params or 'url2' in query_params:
+			message = '<iframe src="' 
+			message += query_params['url1'][0]
+			message += '" width="49%" height="10000" align="left" scrollable="no"></iframe>'
+			message += '<iframe src="'
+			message += query_params['url2'][0]
+			message += '" width="49%" height="10000" align="right" scrollable="no"></iframe>'
+		else:
+			message = ''
 		self._set_headers()
-		self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path, "utf-8"))
-
-
-
-
-
-
+		self.wfile.write(bytes(message, "utf-8"))
+#		self._set_headers()
+#		self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path, "utf-8"))
 	#	POST is for submitting data.
+
 	def do_POST(self):
 
 		print( "incomming http: ", self.path )
